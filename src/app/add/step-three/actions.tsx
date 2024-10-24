dc9fc926cbff7fff737eadc59c0f53def9216525
Your Name <ekka.beaula@gmail.com>
2024-10-24 21:48:36 +0200

    StepThreeFormAction complete

--- src/app/add/step-three/actions.tsx
+++ src/app/add/step-three/actions.tsx
@@ -1,2 +1,23 @@
 "use server";
-export const stepThreeFormAction = () => {};
+
+import { stepThreeSchema } from "@/app/schemas";
+import { AddDealRoutes, FormErrors } from "@/app/types";
+import { redirect } from "next/navigation";
+
+export const stepThreeFormAction = (
+  prevState: FormErrors | undefined,
+  formData: FormData
+): FormErrors | undefined => {
+  const data = Object.fromEntries(formData.entries());
+  const validated = stepThreeSchema.safeParse(data);
+
+  if (!validated.success) {
+    const errors = validated.error.issues.reduce((acc: FormErrors, issue) => {
+      acc[issue.path[0]] = issue.message;
+      return acc;
+    }, {});
+    return errors;
+  } else {
+    redirect(AddDealRoutes.REVIEW_DEAL);
+  }
+};
