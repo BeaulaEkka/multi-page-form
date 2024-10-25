9606ea5a4b2c2558fb4e225bcd0f4fea1a302d41
Your Name <ekka.beaula@gmail.com>
2024-10-25 21:39:59 +0200

    Add Deal

--- src/context/AddDealContext.ts
+++ src/context/AddDealContext.ts
@@ -1,7 +1,18 @@
+import { newDeal } from "@/app/schemas";
 import { createContext } from "react";
 
 type AddDealContextType = {
   newDealData: newDeal;
-  updateNewDealDetails: (dealDetails: Partial<NewDeal>) => void;
+  updateNewDealDetails: (dealDetails: Partial<newDeal>) => void;
+  dataLoaded: boolean;
+  resetLocalStorage: () => void;
 };
-export const AddDealContext = createContext<>(null);
+export const AddDealContext = createContext<AddDealContextType | null>(null);
+export const AddDealContextProvider = ({
+  children,
+}: {
+  children: React.ReactNode;
+}) => {
+  const [newDealData, setNewDealData] = useState<NewDeal>({} as NewDeal);
+  return <AddDealContext.Provider value={}>{children}</AddDealContext.Provider>;
+};
